f58f990fcff0fba09158d5b9affd9e5024f40ace
Ediney Andrade <54506695+Ediiney@users.noreply.github.com>
2026-08-18 07:17:44 -0300

    feat: expose gated gaming labs launcher

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Home from '../page'
 import PlanGuard from './plan-guard'
+import GamingLabsLink from './GamingLabsLink'
 
 export default function ProductPage(){
   return (
     <>
       <Home />
       <PlanGuard />
+      <GamingLabsLink />
     </>
   )
 }
